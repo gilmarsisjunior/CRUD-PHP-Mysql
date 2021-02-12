a5f75805880546940217745f9eb0856431157d2a
Gilmar Silva Santos Junior <gilmar.sisjunior@gmail.com>
2021-02-12 04:10:16 -0300

    Problemas de autenticação resolvidos, próximo passo criar sistema para deletar conta existente

--- public_html/PHP/login.php
+++ public_html/PHP/login.php
@@ -1,19 +1,20 @@
 <?php 
+session_start();
 require_once('database.php');
 
 
 $login = $_POST['login'];
 $senha = $_POST['senha'];
 
-$checar = "SELECT login FROM usuarios WHERE login = '$login' ";
+// checa se existe um usuário cadastrado no banco de dados
+$checar = "SELECT login, senha FROM usuarios WHERE login = '$login' AND senha = '$senha' ";
+            $autenticacao = mysqli_query($connect, $checar);
 
-$autenticacao = mysqli_query($connect, $checar);
-
-if ($autenticacao){
-    echo "usuário encontrado";
-}
-else {
-    echo "usuario não encontrado";
+                    if (mysqli_num_rows($autenticacao) > 0){
+                        echo "usuário encontrado";
+                    }
+                    else {
+                        echo "usuario não encontrado";
 }
 
 ?>
